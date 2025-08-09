b604e7def1ba8949a0ea9ebaf273d06366b0ef04
eddyxwu <edwardwu2017@hotmail.com>
2025-08-09 15:46:30 +0000

    Deploying to gh-pages from @ eddyxwu/eddyxwu.github.io@887232442f6a7440afb9f712052e55c543957e9a 🚀

--- assets/js/google-analytics-setup.js
+++ assets/js/google-analytics-setup.js
@@ -4,3 +4,14 @@ function gtag() {
 }
 gtag("js", new Date());
 gtag("config", "G-RR8BVR62VF");
+
+
+<!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-C9BS271QDJ"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-C9BS271QDJ');
+</script>
