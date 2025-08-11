807f8f9702d5fb5de6a442f9a92ae6ce8537b17d
Edward Wu <edwardwu2017@hotmail.com>
2025-08-10 23:15:38 -0400

    Update google-analytics-setup.js

--- _scripts/google-analytics-setup.js
+++ _scripts/google-analytics-setup.js
@@ -7,14 +7,3 @@ function gtag() {
 }
 gtag("js", new Date());
 gtag("config", "{{ site.google_analytics }}");
-
-
-<!-- Google tag (gtag.js) -->
-<script async src="https://www.googletagmanager.com/gtag/js?id=G-C9BS271QDJ"></script>
-<script>
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
-
-  gtag('config', 'G-C9BS271QDJ');
-</script>
